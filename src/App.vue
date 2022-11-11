4c87365113d429aebed600a881f0e6c9e0d76910
didoee <2554771377@qq.com>
2022-11-11 17:00:35 +0800

    xxxx

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+const value1 = ref('');
+
+const hanleChange = (value) => {
+	console.log(value);
+	value1.value = value;
+};
+</script>
+
+<template>
+	<el-date-picker
+		:value="value1"
+		@change="hanleChange"
+		type="daterange"
+		range-separator="To"
+		start-placeholder="Start date"
+		end-placeholder="End date"
+	/>
+</template>
